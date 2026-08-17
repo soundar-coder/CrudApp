d734e27a4cf23064e3fccdea1c3ec23abd330a6e
srinath <sreesekar999@gmail.com>
2026-08-17 18:13:08 +0530

    Method added

--- src/main/java/com/leanring/crud/CrudApplication.java
+++ src/main/java/com/leanring/crud/CrudApplication.java
@@ -9,5 +9,7 @@ public class CrudApplication {
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
     }
-
+    public void learning(){
+        System.out.println("Srinath is learning");
+    }
 }
